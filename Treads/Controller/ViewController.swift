4ff3486554f3aa0d60450b93b63568a739035ca3
Armand Kamffer <akamffer@mweb.co.za>
2019-06-06 14:34:19 +0200

    Completed OnRun VC screen

---
+++ Treads/Controller/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Treads
+//
+//  Created by Armand Kamffer on 2019/06/06.
+//  Copyright © 2019 Armand Kamffer. All rights reserved.
+//
+
+import UIKit
+
+class OnRunVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
