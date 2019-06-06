4ff71d4cc26b6e7d8fdfa92bc0a1d3dc758322ce
Armand Kamffer <akamffer@mweb.co.za>
2019-06-06 09:22:38 +0200

    Initial Commit

---
+++ Treads/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Treads
+//
+//  Created by Armand Kamffer on 2019/06/06.
+//  Copyright © 2019 Armand Kamffer. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
