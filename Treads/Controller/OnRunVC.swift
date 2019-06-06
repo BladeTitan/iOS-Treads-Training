59589912b98fd947d618ff7a9bd2b8d8352c8c55
Armand Kamffer <akamffer@mweb.co.za>
2019-06-06 15:15:28 +0200

    Added animation for swiping to end running session

--- Treads/Controller/OnRunVC.swift
+++ Treads/Controller/OnRunVC.swift
@@ -8,23 +8,59 @@
 
 import UIKit
 
-class OnRunVC: UIViewController {
-
+class OnRunVC: LocationVC {
+    //***************************************************
+    //MARK:- IBOutlets
+    @IBOutlet weak var timeLbl: UILabel!
+    @IBOutlet weak var averagePaceLbl: UILabel!
+    @IBOutlet weak var distanceLbl: UILabel!
+    @IBOutlet weak var swipeBackgroundImg: UIImageView!
+    @IBOutlet weak var sliderImg: UIImageView!
+    
+    //***************************************************
+    //MARK:- Lifecycle Hook Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let swipeGesture = UIPanGestureRecognizer(target: self, action: #selector(endRunSwiped))
+        sliderImg.addGestureRecognizer(swipeGesture)
+        sliderImg.isUserInteractionEnabled = true
+        swipeGesture.delegate = self
     }
+
+    @objc func endRunSwiped(sender: UIPanGestureRecognizer) {
+        let minAdjust: CGFloat = 78
+        let maxAdjust: CGFloat = 132.5
+        
+        if let sliderView = sender.view {
+            if(sender.state == UIGestureRecognizer.State.changed || sender.state == UIGestureRecognizer.State.began) {
+                let translation = sender.translation(in: self.view)
+                
+                if(sliderView.center.x >= swipeBackgroundImg.center.x - minAdjust && sliderView.center.x <= swipeBackgroundImg.center.x + maxAdjust) {
+                    sliderView.center.x = sliderView.center.x + translation.x
+                }
+                else if(sliderView.center.x >= swipeBackgroundImg.center.x + maxAdjust) {
+                    sliderView.center.x = swipeBackgroundImg.center.x + maxAdjust
+                    
+                    dismiss(animated: true, completion: nil)
+                }
+                else if(sliderView.center.x <= swipeBackgroundImg.center.x + minAdjust) {
+                    UIView.animate(withDuration: 0.1) {
+                        sliderView.center.x = self.swipeBackgroundImg.center.x - minAdjust
+                    }
+                }
+                
+                sender.setTranslation(CGPoint.zero, in: self.view)
+            }
+            else if(sender.state == UIGestureRecognizer.State.ended) {
+                UIView.animate(withDuration: 0.1) {
+                    sliderView.center.x = self.swipeBackgroundImg.center.x - minAdjust
+                }
+            }
+        }
+    }
+}
+
+extension OnRunVC: UIGestureRecognizerDelegate {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
